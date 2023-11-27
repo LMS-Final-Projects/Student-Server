f2227e67a67d8b5682dc865a3d932aa531515eae
cos12378 <yrias8700@gmail.com>
2023-11-27 10:48:44 +0900

    init

---
+++ src/test/java/com/example/studentsserver/StudentsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
